f1fb3cd6cbcb49df78ec5e1c17dd189aaa5ab857
Theo Bofia <tbofia@yahoo.co.uk>
2023-09-18 18:01:33 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+      // This is my first javescritp code
+      console.log('Hello world')
+
+      let name = "Mosh";
+      console.log(name)
+
+      let person = {
+          name: "Josh",
+          Age: 30
+      }
+
+      console.log(person.name)
+
+      console.log(person)
+
+      let selectedColors = ['red','blue']
+
+      console.log(selectedColors)
+
+      console.log(typeof(selectedColors))
+
+      const names = 'john, andrew, peter,   james, colin';
+
+      console.log(names.replace(/\s/g,'').split(','));
